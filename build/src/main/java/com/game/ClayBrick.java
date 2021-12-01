a340575af9bca4cae6ebd78a9a65262565a6faa4
mazbrew <peteryeoh50@gmail.com>
2021-12-01 14:38:09 +0800

    changed some colors

--- build/src/main/java/com/game/ClayBrick.java
+++ build/src/main/java/com/game/ClayBrick.java
@@ -6,14 +6,9 @@ import java.awt.Point;
 public class ClayBrick extends Brick {
 
     private static final String NAME = "Clay Brick";
-    private static final Color DEF_INNER = new Color(178, 34, 34).darker();
+    private static final Color DEF_INNER = new Color(200, 105, 84).darker();
     private static final Color DEF_BORDER = Color.GRAY;
     private static final int CLAY_STRENGTH = 1;
-
-
-
-
-
 
     public ClayBrick(Point point, Dimension size){
         super(NAME,point,size,DEF_BORDER,DEF_INNER,CLAY_STRENGTH);
